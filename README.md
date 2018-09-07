ed2cf6d8aca35b58cb2d41691bbe751deb1251d2
futpib <futpib@gmail.com>
2018-09-07 03:41:09 +0300

    Add telegram channel and bot

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-# breeze-dark-tdesktop-theme
-A telegram desktop theme based on the Breeze Dark color scheme.  
-Based on some other theme i found a long time ago, and my attempt at fixing
-that one (correcting colors and such).  
-In theory, you could download a zip file of the source code, rename the file
-extension to `.tdesktop-theme` and then apply that to Telegram, but don't quote
-me on this.
+# Breeze Dark for Telegram Desktop
+> Telegram desktop theme to match KDE Breeze Dark desktop color scheme.
 
-## Build
+## Download
+
+https://t.me/BreezeDark
+
+Releases are automatically uploaded to this channel.
+
+## Manual build
 
 ```bash
 make
---
+++ bot.js
@@ -0,0 +1,42 @@
+
+const TelegramBot = require('node-telegram-bot-api');
+const Agent = require('socks5-https-client/lib/Agent');
+
+const socksHost = process.env.PROXY_SOCKS5_HOST || 'localhost';
+const socksPort = parseInt(process.env.PROXY_SOCKS5_PORT || '9050', 10);
+
+const commit = process.env.TRAVIS_COMMIT;
+const slug = process.env.TRAVIS_REPO_SLUG;
+
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {
+	polling: {
+		autoStart: false,
+	},
+	request: process.env.PROXY_ENABLE && {
+		agentClass: Agent,
+		agentOptions: {
+			socksHost,
+			socksPort,
+		},
+	},
+});
+
+const caption = (slug && commit) ?
+	`https://github.com/${slug}/commit/${commit}` :
+	'manual build';
+
+async function main() {
+	await bot.startPolling();
+	await bot.sendDocument('@BreezeDark', './breeze-dark.tdesktop-theme', {
+		caption,
+	});
+	await bot.stopPolling();
+}
+
+main().then(
+	() => process.exit(0),
+	err => {
+		console.error(err);
+		process.exit(1);
+	},
+);
